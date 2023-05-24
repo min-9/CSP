88296720f84cbd10c62a614f9b2f96a21e0946c7
min-9 <mg07315@gmail.com>
2023-05-24 21:21:15 +0900

    Apply `metadata` about server-side pages

--- app/layout.js
+++ app/layout.js
@@ -14,28 +14,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Script type="text/x-mathjax-config">
-          {typeof window !== 'undefined' &&
-            window.MathJax.Hub.Config({
-              jax: ['input/TeX', 'output/HTML-CSS'],
-              tex2jax: {
-                inlineMath: [
-                  ['$', '$'],
-                  ['\\(', '\\)'],
-                ],
-                displayMath: [
-                  ['$$', '$$'],
-                  ['\\[', '\\]'],
-                ],
-                processEscapes: true,
-              },
-              'HTML-CSS': { availableFonts: ['TeX'] },
-            })}
-        </Script>
-        <script
-          async
-          src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/MathJax.js?config=TeX-AMS_HTML-full,Safe"
-        ></script>
         <Header />
         {children}
       </body>
